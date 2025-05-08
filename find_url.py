f40f6892555c68fe3a6671939ecb663329f305a2
rafig256 <rafig_256@yahoo.com>
2025-05-08 14:57:30 +0330

    seperate normal_lins and filtered_linnks

--- find_url.py
+++ find_url.py
@@ -1,5 +1,19 @@
+import os
 import xml.etree.ElementTree as ET
-import pandas as pd 
+import pandas as pd
+
+def filter_urls(urls):
+    filter_key = ['استخدام','نتایج','کارنامه','پذیرش','مصاحبه','آزمون']
+    filtered_urls = []
+    normal_urls = []
+
+    for url in urls:
+        if any(keyword in url for keyword in filter_key):
+            filtered_urls.append(url)
+        else:
+            normal_urls.append(url)
+
+    return normal_urls, filtered_urls
 
 def find_urls_from_sitemap(sitemap_path):
     try:
@@ -17,16 +31,30 @@ def find_urls_from_sitemap(sitemap_path):
 # مسیر فایل ورودی sitemap
 sitemap_file_path = 'sitemap.xml'
 
-# مسیر خروجی فایل Excel
-output_excel_path = 'result/sitemap_links.xlsx'
+# مسیر خروجی فایل‌ها
+output_dir = 'result'
+output_excel_path_normal = os.path.join(output_dir, 'sitemap_links.xlsx')
+output_excel_path_filtered = os.path.join(output_dir, 'filtered_links.xlsx')
+
+# اطمینان از وجود پوشه خروجی
+os.makedirs(output_dir, exist_ok=True)
 
 # استخراج لینک‌ها
 urls = find_urls_from_sitemap(sitemap_file_path)
 
-# اگر لیستی از لینک‌ها بود، ذخیره در Excel
+# اعمال فیلترهای دلخواه
 if isinstance(urls, list):
-    df = pd.DataFrame(urls, columns=["URL"])
-    df.to_excel(output_excel_path, index=False)
-    print(f"{len(urls)} لینک در فایل '{output_excel_path}' ذخیره شد.")
+    urls, filtered_urls = filter_urls(urls)
+
+    # ذخیره لینک‌های معمولی
+    df_normal = pd.DataFrame(urls, columns=["URL"])
+    df_normal.to_excel(output_excel_path_normal, index=False)
+
+    # ذخیره لینک‌های فیلترشده
+    df_filtered = pd.DataFrame(filtered_urls, columns=["Filtered URL"])
+    df_filtered.to_excel(output_excel_path_filtered, index=False)
+
+    print(f"{len(urls)} لینک معمولی در '{output_excel_path_normal}' ذخیره شد.")
+    print(f"{len(filtered_urls)} لینک فیلترشده در '{output_excel_path_filtered}' ذخیره شد.")
 else:
-    print(urls)  # چاپ پیام خطا در صورت نیاز
+    print(urls)  # چاپ پیام خطا
