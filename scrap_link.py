b733f332d90ce4b75ba334bffaea1e273cf89e5b
rafig256 <rafig_256@yahoo.com>
2025-05-09 20:11:21 +0330

    complete scrap only p tag

--- scrap_link.py
+++ scrap_link.py
@@ -1,18 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import os
 
-url = 'https://www.heyvagroup.com/shownews/7790/%DA%A9%D9%86%DA%A9%D9%88%D8%B1.html'
+url = 'https://www.heyvagroup.com/shownews/9971/%D8%AA%D9%85%D8%AF%DB%8C%D8%AF-%D9%88-%D8%AA%D8%B9%D9%88%DB%8C%D8%B6-%DA%A9%D8%A7%D8%B1%D8%AA-%D9%85%D9%87%D8%A7%D8%B1%D8%AA-%D9%81%D9%86%DB%8C-%D8%AD%D8%B1%D9%81%D9%87-%D8%A7%DB%8C.html'
 
 r = requests.get(url)
-
-soup = BeautifulSoup(r.content , 'html.parser')
-
+soup = BeautifulSoup(r.content, 'html.parser')
 ps = soup.find_all('p')
 
-count = 0  # تعریف یک متغیر شمارنده با مقدار اولیه صفر
+count = 0
+data_p_tag = []
+min_word_count = 15
 
 for p in ps:
-    text = p.text.strip()  # حذف space ها، newline ها و tab ها از ابتدا و انتهای متن
-    if text:  # اگر متن بعد از حذف فضاهای خالی، چیزی داشت چاپ کن
-        count +=1
-        print(str(count) + ": " + text)  # تبدیل عدد به رشته با str()
+    text = p.text.strip()
+    words = text.split()  # جدا کردن متن به کلمات
+    if len(words) >= min_word_count:
+        count += 1
+        data_p_tag.append(text)
+        print(str(count) + ": " + text)
+
+print(f"تعداد پاراگراف‌های واجد شرایط: {count}")
+
+# ایجاد پوشه result اگر وجود ندارد
+output_directory = 'result'
+os.makedirs(output_directory, exist_ok=True)
+output_file = os.path.join(output_directory, 'data.xlsx')
+
+# ذخیره داده‌ها در فایل Excel
+df = pd.DataFrame({'متن پاراگراف': data_p_tag})
+df.to_excel(output_file, index=False)  # حذف آرگومان encoding
+
+print(f"محتوای پاراگراف‌های واجد شرایط در فایل '{output_file}' ذخیره شد.")
